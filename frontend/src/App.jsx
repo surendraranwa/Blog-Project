e379ab14f897a60e84457f5f9ae3e51310086cb5
surendraranwa <surendraranwa20@gmail.com>
2024-11-07 22:34:06 +0530

    Header Section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import About from './Pages/About';
@@ -6,10 +6,12 @@ import Dashboard from './Pages/Dashboard';
 import Projects from './Pages/Projects';
 import SignIn from './Pages/SignIn';
 import SignUp from './Pages/SignUp';
+import Header from './Components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/about" element={<About/>}/>
