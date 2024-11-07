5c09c72b758a0dd3c2fbc3447ae1841a1c3302fd
surendraranwa <surendraranwa20@gmail.com>
2024-11-07 20:59:01 +0530

    Created Pages and Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Dashboard from './Pages/Dashboard';
+import Projects from './Pages/Projects';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
 
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/projects" element={<Projects/>}/>
+      <Route path="/signin" element={<SignIn/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
