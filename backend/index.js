224de436e627adfc59ccea8cebc6e3f08a39de75
surendraranwa <surendraranwa20@gmail.com>
2024-11-08 22:35:02 +0530

    Connected WIth MONGODB Database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
-app.listen(5000, ()=>{
-    console.log("Your Server is Running on Port 5000.")
-})
+app.listen(4000, ()=>{
+    console.log("Your Server is Running on Port 4000.")
+});
+
+mongoose.connect(process.env.MONGODB_URL)
+.then(()=>{
+    console.log("Succesfully Connected")
+}).catch((err)=>{
+    console.error(err)
+});
+
+// console.log("MongoDB URL:", process.env.MONGODB_URL);
